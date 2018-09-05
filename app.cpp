85767a3fdad3db0715052542ed5186409b230416
RAMOS João Paulo <jpramos@europe.corp.altran.com>
2018-09-05 18:43:08 +0100

    Update writing map

--- app.cpp
+++ app.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
+
+void writing_map (string variable_name, string variable_type){
+	
+	ofstream outfile("teste.xml");
+		cout << "Opening File XML"<< endl;
+	outfile << variable_name << " " << variable_type << endl;
+
+	outfile.close();
+}
 
 void ficheiro_method(){
 	
@@ -38,20 +48,23 @@ void ficheiro_method(){
 					found3 = s.find(" type");
 			//		cout << found3 << endl;
 					string variable_name = s.substr (found2+6,found3-found2-7);
-					cout << str2 << endl;
-				}
-				found2 = s.find(aux3);
-				if (found2!=string::npos)
-				{
-					char a = '"';
-			//		cout << found2 << endl;
-					found3 = s.find(" />");
-				//	cout << found3 << endl;
-					if (found3!=string::npos){
-						string variable_type = s.substr (found2+10,found3-found2-11);
-						cout << str2 << endl;
+					cout << variable_name << endl;
+				
+					found2 = s.find(aux3);
+					if (found2!=string::npos)
+					{
+						char a = '"';
+				//		cout << found2 << endl;
+						found3 = s.find(" />");
+					//	cout << found3 << endl;
+						if (found3!=string::npos){
+							string variable_type = s.substr (found2+10,found3-found2-11);
+							cout << variable_type << endl;
+							writing_map(variable_name,variable_type);
+						}
+						
 					}
-				}	
+				}				
 			}
 			getline(f1,s);
 			
@@ -61,8 +74,6 @@ void ficheiro_method(){
 		cout << "Error opening File"<< endl;
 	}
 }
-
-//void writing_XML
 
 
 int main(){
